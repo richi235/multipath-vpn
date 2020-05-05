3844722ed9617fbb8ba06db2f838e1a949f9a2de
Richard Sailer <richard_siegfried@systemli.org>
2020-05-05 02:05:59 +0200

    Better newlines to semantically group log messages

--- minimal_experiment_prototype.pl
+++ minimal_experiment_prototype.pl
@@ -463,7 +463,7 @@ sub select_adaptively
             $opti_sock_id = $subtun_hash->{sock_id};
         }
     }
-    $ALGOLOG->NOTICE("selected sock: $opti_sock_id, with weighted_fill: $min_weighted_fill\n");
+    $ALGOLOG->NOTICE("selected sock: $opti_sock_id, with weighted_fill: $min_weighted_fill");
     return $opti_sock_id;
 }
 # TODOS:
@@ -583,7 +583,7 @@ sub send_scheduler_afmt_fl
         # also updates the real array in %flow_table
         $value_array->[0] = $opti_sock_id;
         $value_array->[1] = time();
-        $ALGOLOG->NOTICE("send_scheduler_afmt_fl(): continuing existing flow, using sock id: $opti_sock_id");
+        $ALGOLOG->NOTICE("send_scheduler_afmt_fl(): continuing existing flow, using sock id: $opti_sock_id\n");
         $poe_kernel->call( $subtun_sessions[$opti_sock_id], "on_data_to_send", $_[0] );
         return;
     } else { # packet starts a new flow
