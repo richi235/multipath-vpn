8b8afa8c31759be97f396205a856eb58020657d5
Richard Sailer <richard_siegfried@systemli.org>
2020-06-30 22:34:53 +0200

    Update send_rate scaling in logging

--- minimal_experiment_prototype.pl
+++ minimal_experiment_prototype.pl
@@ -493,9 +493,10 @@ sub select_adaptively
             ( ( ($subtun_hash->{sock_fill}*$subtun_hash->{sock_fill}) + $packet_size) /
                   ($subtun_hash->{send_rate} || 1) )
             * $subtun_hash->{srtt};
+#        say(colored($weighted_fill, "bold red"));
         $ALGOLOG->NOTICE("select_adaptively(): sock_id: $subtun_hash->{sock_id}"
                            . " | srtt:" . $subtun_hash->{srtt}/1000 . "ms"
-                           . " | send_rate:" . ($subtun_hash->{send_rate} >> 6)/1000 . "kB/s"
+                           . " | send_rate:" . ($subtun_hash->{send_rate}) . " (B/s)"
 #                           . " | calc send_rate:" . ($subtun_hash->{calc_rate})/1000 . "kB/s"
                            . " | sock_fill: $subtun_hash->{sock_fill} Byte"
                            . " | resulting weighted_fill: $weighted_fill");
