f0da305caa17cedaff31c53162fef52856add2ec
Richard Sailer <richard_siegfried@systemli.org>
2020-08-14 14:18:57 +0200

    Fix: enter_texit.sh also entered t_entry, also nicer enter scripts
    Because there was only one tmux server that was already running in t_entry
    netwrok namespace

--- ip_netns/enter_tentry.sh
+++ ip_netns/enter_tentry.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ip netns exec T_entry tmux new-session \
-   'echo -e "\nUse: ./minimal_experiment_prototype.pl -c /etc/T_entry.cfg \n" ; and fish'
+   'echo -e "\nUse: ../minimal_experiment_prototype.pl -c T_entry.cfg --ccid=2 --sched=otias_sock_drop \n" ; and fish'
--- ip_netns/enter_texit.sh
+++ ip_netns/enter_texit.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-ip netns exec T_exit tmux new-session \
-   'echo -e "\nUse: ./minimal_experiment_prototype.pl -c /etc/T_exit.cfg \n" ; and fish'
+ip netns exec T_exit byobu
