f10f17636603ff2c75c6e6747c0447a99777c318
Richard Sailer <richard_siegfried@systemli.org>
2020-08-26 23:18:20 +0200

    namespaces: introduce 3 veths and tc netem

--- ip_netns/setup_namespaces_and_network.sh
+++ ip_netns/setup_namespaces_and_network.sh
@@ -7,17 +7,42 @@ modprobe dccp_ipv4
 ip netns add T_exit
 ip netns add T_entry
 
-# Create two interconnected veth interfaces
-ip link add veth0 type veth peer name veth1
+# Create 6 pairwise interconnected veth interfaces
+ip link add veth11 type veth peer name veth12
+ip link add veth21 type veth peer name veth22
+ip link add veth31 type veth peer name veth32
 
-# Put the two interfaces into the two namespaces
-ip link set veth0 netns T_entry
-ip link set veth1 netns T_exit
+# Put the interfaces into the respective namespaces
+ip link set veth11 netns T_entry
+ip link set veth21 netns T_entry
+ip link set veth31 netns T_entry
 
-# Configure the interface in T_entry
-ip netns exec T_entry ip addr add 10.7.7.1/24 dev veth0
-ip netns exec T_entry ip link set veth0 up
+ip link set veth12 netns T_exit
+ip link set veth22 netns T_exit
+ip link set veth32 netns T_exit
 
-# Configure the interface in T_exit
-ip netns exec T_exit ip addr add 10.7.7.2/24 dev veth1
-ip netns exec T_exit ip link set veth1 up
+# Configure the interfaces in T_entry
+ip netns exec T_entry ip addr add 1.0.0.1/24 dev veth11
+ip netns exec T_entry ip link set veth11 up
+tc -netns T_entry qdisc add dev veth11 root handle 20: netem delay 20ms rate 16mbit
+
+ip netns exec T_entry ip addr add 2.0.0.1/24 dev veth21
+ip netns exec T_entry ip link set veth21 up
+tc -netns T_entry qdisc add dev veth21 root handle 20: netem delay 35ms rate 16mbit
+
+ip netns exec T_entry ip addr add 3.0.0.1/24 dev veth31
+ip netns exec T_entry ip link set veth31 up
+tc -netns T_entry qdisc add dev veth31 root handle 20: netem delay 25ms rate 32mbit
+
+# Configure the interfaces in T_exit
+ip netns exec T_exit ip addr add 1.0.0.2/24 dev veth12
+ip netns exec T_exit ip link set veth12 up
+tc -netns T_exit qdisc add dev veth12 root handle 20: netem delay 20ms rate 16mbit
+
+ip netns exec T_exit ip addr add 2.0.0.2/24 dev veth22
+ip netns exec T_exit ip link set veth22 up
+tc -netns T_exit qdisc add dev veth22 root handle 20: netem delay 35ms rate 16mbit
+
+ip netns exec T_exit ip addr add 3.0.0.2/24 dev veth32
+ip netns exec T_exit ip link set veth32 up
+tc -netns T_exit qdisc add dev veth32 root handle 20: netem delay 25ms rate 32mbit
