d9b44c59f60c15414501c305a6a73c1ecf6c8c7b
Richard Sailer <richard_siegfried@systemli.org>
2020-10-09 17:01:37 +0200

    for experiment, modify paths

--- ip_netns/setup_namespaces_and_network.sh
+++ ip_netns/setup_namespaces_and_network.sh
@@ -28,7 +28,7 @@ tc -netns T_entry qdisc add dev veth11 root handle 20: netem delay 20ms rate 16m
 
 ip netns exec T_entry ip addr add 2.0.0.1/24 dev veth21
 ip netns exec T_entry ip link set veth21 up
-tc -netns T_entry qdisc add dev veth21 root handle 20: netem delay 35ms rate 16mbit
+tc -netns T_entry qdisc add dev veth21 root handle 20: netem delay 25ms rate 16mbit
 
 ip netns exec T_entry ip addr add 3.0.0.1/24 dev veth31
 ip netns exec T_entry ip link set veth31 up
@@ -41,7 +41,7 @@ tc -netns T_exit qdisc add dev veth12 root handle 20: netem delay 20ms rate 16mb
 
 ip netns exec T_exit ip addr add 2.0.0.2/24 dev veth22
 ip netns exec T_exit ip link set veth22 up
-tc -netns T_exit qdisc add dev veth22 root handle 20: netem delay 35ms rate 16mbit
+tc -netns T_exit qdisc add dev veth22 root handle 20: netem delay 25ms rate 16mbit
 
 ip netns exec T_exit ip addr add 3.0.0.2/24 dev veth32
 ip netns exec T_exit ip link set veth32 up
