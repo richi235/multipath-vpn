6124dce7cfc152a01f04c614a918be9a2e43f985
Richard Sailer <richard_siegfried@systemli.org>
2020-10-30 01:19:29 +0100

    run_experiment.sh: timing tweakings, switch to iperf3, other updates

--- net-experiments/run_experiment.sh
+++ net-experiments/run_experiment.sh
@@ -2,8 +2,8 @@
 # This runs a experiment with einsfroest. And generates some graphs from it
 # (packet delay variation and throughput). This is the server side (T_exit)
 
-results_dir="${runtime}s_${sched_algo}_${udp_flag}_${flowcount}flows_${bandwith_opt}_${hdr_opt}_$run"   # the dir the results will be stored in
-echo $results_dir
+results_dir="${runtime}s_${sched_algo}_${udp_flag}_${flowcount}flows_${bandwith_opt}_${hdr_opt}"   # the dir the results will be stored in
+echo -e "\e[31;1m$results_dir \e[0m"
 mkdir $results_dir
 #udp_flag=${udp_flag}" -l 1392" # added after mkdir because spaces are hard + unneeded info in dir name
 
@@ -11,22 +11,23 @@ mkdir $results_dir
 tentry_ssh_dest="root@tentry"
 other_ctx_prefix="ssh $tentry_ssh_dest"
 
-timeout $((runtime+7)) ../minimal_experiment_prototype.pl  \
+timeout $((runtime+28)) ../minimal_experiment_prototype.pl  \
               --sched=$sched_algo $hdr_opt --ccid=2 > $results_dir/texit_logs  &
 sleep 1
 
 #timeout $((runtime+5)) tcpdump -i tun0 -w afmt_tun0_trace.pcap "dst 192.168.65.2" &
 # timeout $((runtime+5)) tcpdump -i veth12 -w afmt_veth12.pcap "proto dccp" &
-timeout $((runtime+4)) iperf $udp_flag  -s -i 0.1 --reportstyle C > iperf_server_output.csv &
+#iperf3 $udp_flag  -s -i 0.1 --reportstyle C > iperf_server_output.csv &
+iperf3 $udp_flag  -s -i 0.1  > iperf_server_output.log  &
 
-$other_ctx_prefix "timeout $((runtime+4)) ~/Coding/Reinhard-VPN/minimal_experiment_prototype.pl \
+$other_ctx_prefix "timeout $((runtime+26)) ~/Coding/Reinhard-VPN/minimal_experiment_prototype.pl \
              --sched=$sched_algo --ccid=2 \
             --lcon=INFO  --lalgo=NOTICE  $hdr_opt --lsci=NOTICE  > tentry_logs" &
 sleep 1
-$other_ctx_prefix "iperf $udp_flag  -t $runtime $bandwith_opt  \
-		            -e -c 192.168.65.2 -i 0.1  -P $flowcount  > iperf_tentry.log" &
+$other_ctx_prefix "iperf3 $udp_flag  -t $runtime $bandwith_opt  \
+		            -c 192.168.65.2 -i 0.1  -P $flowcount  > iperf_tentry.log" &
 
-sleep $((runtime+3))
+sleep $((runtime+27))
 
 # todo: nachdenken über file locations
 # idealerweise sind die commandos ja gleich für alle
@@ -40,7 +41,7 @@ sleep $((runtime+3))
 # if we used ssh copy log files from other host to us:
 # uses bash regex matching, checks if prefx starts with "ssh "
 if [[ $other_ctx_prefix =~ ^ssh[[:blank:]]  ]] ; then
-    scp "$tentry_ssh_dest:{*tentry*log*,time_inflight*.tsv}" .
+    scp -q "$tentry_ssh_dest:{*tentry*log*,time_inflight*.tsv}" .
 fi
 
 #./get_delay_variations.py afmt_tun0_trace.pcap > delay_variations
@@ -48,8 +49,20 @@ fi
 
 gnuplot all_subtuns_time_inflight_cwnd.plt # tunnel internal infos
 #gnuplot packet_delay_variation_plot.plt
-gnuplot throughput2.plt   SRTT_boxplot.plt
+# gnuplot throughput2.plt   SRTT_boxplot.plt
 # grep --fixed-strings "tc -netns" ../ip_netns/setup_namespaces_and_network.sh > $results_dir/network_conf
 # rm afmt_tun0_trace.pcap
 # rm *.tsv
-mv *.tsv all_subtuns_time_inflight_cwnd.pdf SRTTs.pdf  tentry_logs iperf_server_output.csv iperf_tentry.log  delay_variations afmt_pdv.pdf Throughput.pdf $results_dir
+mv *.tsv all_subtuns_time_inflight_cwnd.pdf  tentry_logs iperf_server_output.log iperf_tentry.log $results_dir
+# delay_variations afmt_pdv.pdf Throughput.pdf SRTTs.pdf
+
+
+#exit
+
+mv $results_dir $series_dir
+
+sleep 5s
+pkill iperf3 # kill leftover iperf3 server on texit if it still exists
+sleep 2s
+killall iperf3
+sleep 10s
