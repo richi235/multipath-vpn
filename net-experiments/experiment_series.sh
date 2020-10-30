1bd3b89f20f33652d0e1ee940f9e0042a867b8f4
Richard Sailer <richard_siegfried@systemli.org>
2020-10-30 01:20:05 +0100

    experiment_series: better graphics, comment out rr

--- net-experiments/experiment_series.sh
+++ net-experiments/experiment_series.sh
@@ -4,35 +4,48 @@
 # to have a series of experiments. This simplifies doing science and makes it more comfortable :)
 
 # Sourcing means the current shell will execute the script instead of starting a new bash 
+# Every experimnt script will place its result dir into our $series_dir by itself
 
-
-runtime=30
+runtime=60
 flowcount=6
 run=r1
+
+series_dir="series_${runtime}s_${flowcount}flows_${run}"
 udp_flag= #"-u"
 bandwith_opt= #"-b20m"
-hdr_opt= #"-hdr"
+hdr_opt="-hdr"
+
+mkdir $series_dir
 
 sched_algo=llfmt_noqueue_busy_wait  
-source ./run_experiment.sh
+echo -en "\e[32;1m[1/6]  \e[0m"
+source ./run_experiment.sh 
 
 sched_algo=afmt_noqueue_busy_wait
+echo -en "\e[32;1m[2/6]  \e[0m"
 source ./run_experiment.sh
 
 sched_algo=afmt_noqueue_drop
+echo -en "\e[32;1m[3/6]  \e[0m"
 source ./run_experiment.sh
 
 sched_algo=afmt_fl
+echo -en "\e[32;1m[4/6]  \e[0m"
 source ./run_experiment.sh
 
 sched_algo=otias_sock_drop
+echo -en "\e[32;1m[5/6]  \e[0m"
 source ./run_experiment.sh
 
 sched_algo=srtt_min_busy_wait
+echo -en "\e[32;1m[6/6]  \e[0m"
 source ./run_experiment.sh
 
-sched_algo=rr
-source ./run_experiment.sh
+#sched_algo=rr
+#source ./run_experiment.sh
 
+cd $series_dir
+tail -n 4 */iperf_tentry.log > iperf_client_sums
+tail -n 4 */iperf_server_output.log > iperf_server_sums
 
-
+echo -e "\e[31;1mSeries dir: $series_dir\e[0m"
