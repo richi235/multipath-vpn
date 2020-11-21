27b609ef2ced5953e4cbcc558b3f6744afb5e667
Richard Sailer <richard_siegfried@systemli.org>
2020-11-21 05:12:26 +0100

    move 2 .plt files into the experiment_series script as here doc
    + change $1 to column(1)

--- net-experiments/experiment_series.sh
+++ net-experiments/experiment_series.sh
@@ -56,7 +56,54 @@ cd $series_dir
 tail -n 4 */iperf_tentry.log > iperf_client_sums
 # tail -n 4 */iperf_server_output.log > iperf_server_sums
 
-gnuplot ../all_throughput_intvervals_boxplots.plt ../all_SRTT_boxplots.plt
+# all_SRTT_boxplots.plt
+gnuplot  <<PLOT
+# This generates boxplot diagramms of the RTTs directly from the iperf output files
+set terminal pdf
+set output "SRTTs.pdf"
+set title "SRTTs"
+set ylabel "ms"
+unset xlabel
+
+set style fill solid 0.5 border -1
+set style data boxplot
+set style boxplot nooutliers
+#set boxwidth  0.5
+
+plot '<grep -P -o "(?<=K/)(\d*)(?= us )" afmt_noqueue_drop/iperf_tentry.log' using (0.5):(column(1)/1000) title 'afmt\_noqueue\_drop' \
+, '<grep -P -o "(?<=K/)(\d*)(?= us )" afmt_fl/iperf_tentry.log' using (1.5):(column(1)/1000) title 'afmt\_fl'\
+, '<grep -P -o "(?<=K/)(\d*)(?= us )" otias_sock_drop/iperf_tentry.log' using (2.5):(column(1)/1000) title 'otias\_sock\_drop' \
+, '<grep -P -o "(?<=K/)(\d*)(?= us )" srtt_min_busy_wait/iperf_tentry.log' using (3.5):(column(1)/1000) title 'srtt\_min\_busy\_wait' \
+, '<grep -P -o "(?<=K/)(\d*)(?= us )" afmt_noqueue_busy_wait/iperf_tentry.log' using (4.5):(column(1)/1000) title 'afmt\_noqueue\_busy\_wait' \
+, '<grep -P -o "(?<=K/)(\d*)(?= us )" llfmt_noqueue_busy_wait/iperf_tentry.log' using (5.5):(column(1)/1000) title 'llfmt\_noqueue\_busy\_wait'
+
+# about: (0.5):(column(1)/1000)
+# the 0.5 is the x value where this boxplot box will be placed
+# column(1) is the column of the data file used , 0 would be the record number
+PLOT
+
+# all_throughput_intvervals_boxplots.plt
+gnuplot  <<PLOT
+# This generates a boxplot diagrmm of the Throughputs per 0.1s interval directly from the iperf output
+
+set terminal pdf
+set output "Throughputs_per_interval.pdf"
+set title "Throughputs per interval"
+set ylabel "Mbit/s"
+unset xlabel
+
+set style fill solid 0.5 border -1
+set style data boxplot
+set style boxplot nooutliers
+#set boxwidth  0.5
+
+plot '<grep -v -e "^\[SUM\]" -e "0\.0000-[1-9][0-9]"  afmt_noqueue_drop/iperf_tentry.log |  grep -P -o "( \d+\.\d+)(?= Mbits/sec )"' using (0.5):(column(1)) title 'afmt\_noqueue\_drop' \
+, '<grep -v -e "^\[SUM\]" -e "0\.0000-[1-9][0-9]"  afmt_fl/iperf_tentry.log |  grep -P -o "( \d+\.\d+)(?= Mbits/sec )"' using (1.5):(column(1)) title 'afmt\_fl'\
+, '<grep -v -e "^\[SUM\]" -e "0\.0000-[1-9][0-9]"  otias_sock_drop/iperf_tentry.log |  grep -P -o "( \d+\.\d+)(?= Mbits/sec )"' using (2.5):(column(1)) title 'otias\_sock\_drop' \
+, '<grep -v -e "^\[SUM\]" -e "0\.0000-[1-9][0-9]"  srtt_min_busy_wait/iperf_tentry.log |  grep -P -o "( \d+\.\d+)(?= Mbits/sec )"' using (3.5):(column(1)) title 'srtt\_min\_busy\_wait' \
+, '<grep -v -e "^\[SUM\]" -e "0\.0000-[1-9][0-9]"  afmt_noqueue_busy_wait/iperf_tentry.log |  grep -P -o "( \d+\.\d+)(?= Mbits/sec )"' using (4.5):(column(1)) title 'afmt\_noqueue\_busy\_wait' \
+, '<grep -v -e "^\[SUM\]" -e "0\.0000-[1-9][0-9]"  llfmt_noqueue_busy_wait/iperf_tentry.log |  grep -P -o "( \d+\.\d+)(?= Mbits/sec )"' using (5.5):(column(1)) title 'llfmt\_noqueue\_busy\_wait'
+PLOT
 
 # Write a file with our path config
 echo "ig0:" 		   		> path_config
